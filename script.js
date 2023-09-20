9b8944027d2dcb1abd731e1a102c0179cc895820
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-20 18:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const enterButton = document.getElementById("enterBtn");
+const statusParagraph = document.getElementById("status");
+
+// Function to change the text in the p tag
+function changeText() {
+    const h1Tag = document.createElement("h1");
+    h1Tag.textContent = "Entered Metaverse";
+    statusParagraph.innerHTML = ''; // Clear any existing content
+    statusParagraph.appendChild(h1Tag); // Append the new h1 tag
+}
+
+// Add event listener to the button
+enterButton.addEventListener("click", changeText);
