bd57d5f5a1c71e1a30f4fd4a53c30640994ae137
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-21 21:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Text</title>
-</head>
-<body>
-    <p id="status">Enter the Metaverse</p>
-    <button id="enterBtn" onclick="changeText()">Enter</button>
 
-    <script>
-        function changeText() {
-            document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
-        }
-    </script>
-</body>
-</html>
+       document.addEventListener("DOMContentLoaded", function () {
+    const enterBtn = document.getElementById("enterBtn");
+    const status = document.getElementById("status");
+
+    enterBtn.addEventListener("click", function () {
+        status.innerHTML = "<h1>Entered Metaverse</h1>";
+    });
+});
